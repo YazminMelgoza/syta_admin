34996ab970ff42e431a6d26b9dd09a6bd22352e5
MarioMedePrsc <rioma.mercury@gmail.com>
2024-04-25 00:34:29 -0600

    process to add an inspection full added

--- lib/screens/add_inspection_choose_car.dart
+++ lib/screens/add_inspection_choose_car.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:syta_admin/model/administrator_model.dart';
 import 'package:syta_admin/screens/main_screen.dart';
+import 'package:syta_admin/screens/add_inspection_create.dart';
+
 
 
 class AddInspectionCar extends StatefulWidget {
@@ -14,11 +17,30 @@ class AddInspectionCar extends StatefulWidget {
 class _AddInspectionCarState extends State<AddInspectionCar> {
   final TextEditingController _searchController = TextEditingController();
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  String userId = "";
 
+  Future<void> getUserId(userPhone) async
+  {
+    try {
+      await _firebaseFirestore.collection("users").where("phoneNumber", isEqualTo: userPhone).get().then(
+            (querySnapshot) {
+          print("Successfully completed");
+          for (var docSnapshot in querySnapshot.docs) {
+            print('${docSnapshot.id} => ${docSnapshot.data()}');
+            userId = docSnapshot.id;
+          }
+        },
+        onError: (e) => print("Error completing: $e"),
+      );
 
+    } catch (e) {
+      print("Error obteniendo el documento: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         foregroundColor: Colors.white,
@@ -49,88 +71,126 @@ class _AddInspectionCarState extends State<AddInspectionCar> {
                   fontWeight: FontWeight.bold, // Texto en negrita
                 )
             ),
-            SizedBox(height: 10,),
-            Text("Paso 2. Selecciona el carro", textAlign: TextAlign.left,
+            const SizedBox(height: 10,),
+            Text("Paso 2. Selecciona el carro",
+                textAlign: TextAlign.left,
                 style: TextStyle(
                   fontSize: 24, // Tamaño de la letra en puntos
                   fontWeight: FontWeight.bold, // Texto en negrita
                 )
             ),
             SizedBox(height: 20,),
-            //Lista de carros
-            StreamBuilder<QuerySnapshot>(
-              stream: _firebaseFirestore.collection('cars').snapshots(),
-              builder: (context, snapshot)
-              {
+            FutureBuilder<void>(
+              future: Future.wait([getUserId(widget.numero)]),
+              builder: (context, snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting)
                 {
-                  return Center(child: CircularProgressIndicator());
+                  return const CircularProgressIndicator();
                 }
                 if (snapshot.hasError)
                 {
-                  return Text('Error al obtener los datos: ${snapshot.error}');
+                  return Text('Error: ${snapshot.error}');
                 }
-                if (!snapshot.hasData)
-                {
-                  return Text('No hay documentos disponibles');
-                }
-                List<QueryDocumentSnapshot> cars = snapshot.data!.docs;
-                return Expanded(
-                  child: ListView.builder(
-                    itemCount: cars.length,
-                    itemBuilder: (context, index) {
-                      Map<String, dynamic> carData = cars[index].data() as Map<String, dynamic>;
-                      String documentId = cars[index].id;
-                      String model = carData['model'];
-                      String name = carData['name'];
-                      String plates = carData['plates'];
+                return (userId != "") ?  StreamBuilder<QuerySnapshot>(
+                  stream: _firebaseFirestore.collection('cars').where("actualUserId", isEqualTo: userId).snapshots(),
+                  builder: (context, snapshot)
+                  {
+                    if (snapshot.connectionState == ConnectionState.waiting)
+                    {
+                      print(userId);
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    if (snapshot.hasError)
+                    {
+                      return Text('Error al obtener los datos: ${snapshot.error}');
+                    }
+                    if (!snapshot.hasData)
+                    {
+                      return Text('No hay documentos disponibles');
+                    }
 
+                    List<QueryDocumentSnapshot> cars = snapshot.data!.docs;
+                    return Expanded(
+                      child: ListView.builder(
+                        itemCount: cars.length,
+                        itemBuilder: (context, index) {
+                          Map<String, dynamic> carData = cars[index].data() as Map<String, dynamic>;
+                          String carId = cars[index].id;
+                          String model = carData['model'];
+                          String name = carData['name'];
+                          String plates = carData['plates'];
 
-                      return Center(
-                        child: Container(
-                          //width: 200,
-                          margin: EdgeInsets.all(10),
-                          padding: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            color: Colors.greenAccent, // Color de fondo del Container
-                            borderRadius: BorderRadius.circular(10), // Radio de borde del Container
-                          ),
-                          child: Row(
-                            //mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              IconButton(
-                                onPressed: () {
-
-                                },
-                                icon: Icon(Icons.car_repair_outlined),
-                                iconSize: 32,
+                          return Center(
+                            child: Container(
+                              //width: 200,
+                              margin: EdgeInsets.all(10),
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                color: Colors.greenAccent, // Color de fondo del Container
+                                borderRadius: BorderRadius.circular(10), // Radio de borde del Container
                               ),
-                              SizedBox(width: 10),
-                              GestureDetector(
-                                  onTap: () {
+                              child: Row(
+                                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  IconButton(
+                                    onPressed: () {
 
-                                  },
-                                  child: Container(
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Text(name,
-                                          style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,),),
-                                        Text("Modelo: "+model, style: TextStyle(fontSize: 12),textAlign: TextAlign.left,),
-                                        Text("Placas: "+plates, style: TextStyle(fontSize: 12),textAlign: TextAlign.left,),
-                                      ],
-                                    ),
-                                  )
+                                    },
+                                    icon: Icon(Icons.car_repair_outlined),
+                                    iconSize: 32,
+                                  ),
+                                  SizedBox(width: 10),
+                                  GestureDetector(
+                                      onTap: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) => CreateInspection(
+                                              carId: carId,
+                                              userId: userId,
+                                            ),
+                                          ),
+                                        );
+
+                                      },
+                                      child: Container(
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text(name,
+                                              style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold,),),
+                                            Text("Modelo: "+model, style: TextStyle(fontSize: 12),textAlign: TextAlign.left,),
+                                            Text("Placas: "+plates, style: TextStyle(fontSize: 12),textAlign: TextAlign.left,),
+                                          ],
+                                        ),
+                                      )
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                        ),
-                      );
-                    },
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ) :
+                Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("El usuario no existe",
+                          style: TextStyle(
+                            fontSize: 24, // Tamaño de la letra en puntos
+                            fontWeight: FontWeight.bold, // Texto en negrita
+                          )
+                      ),
+                      ElevatedButton(onPressed: (){}, child: Text("Crear cuenta a Usuario"))
+                    ],
                   ),
                 );
-              },
-            ),
+          },
+        ),
           ],
         ),
       ),
---
+++ lib/screens/add_inspection_create.dart
@@ -0,0 +1,258 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:syta_admin/screens/add_inspection_choose_car.dart';
+import 'package:syta_admin/screens/main_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:syta_admin/provider/auth_provider.dart';
+
+class CreateInspection extends StatefulWidget {
+  final String carId;
+  final String userId;
+  const CreateInspection({super.key, required this.carId, required this.userId});
+
+  @override
+  State<CreateInspection> createState() => _CreateInspectionState();
+}
+
+class _CreateInspectionState extends State<CreateInspection> {
+  final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  //Input Variables
+  final TextEditingController description = TextEditingController();
+  final TextEditingController estimatedDate = TextEditingController();
+  final TextEditingController title = TextEditingController();
+
+  //Automatic Variables
+  final String endDate = "";
+  final String startDate = "";
+  final String locationId = "";
+  final String status = "EN PROGRESO";
+  //Functions
+  Future<void> _selectDate() async
+  {
+    DateTime? _picked = await showDatePicker
+      (
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2100)
+      );
+    if(_picked != null)
+    {
+      estimatedDate.text = _picked.toString().split(" ")[0];
+
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ap = Provider.of<AuthProvider>(context, listen: false);
+    return Scaffold(
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text("Agregar Revisión", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600),),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MainScreen(),
+                  ), (route) => false);
+            },
+            icon: const Icon(Icons.home, color: Colors.white),
+          ),
+
+        ],
+      ),
+      body: Container(
+        margin: EdgeInsets.all(30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Row(
+              children: [
+                Expanded(
+                  child: Text("Paso 3. Rellena el Formulario:", textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 24, // Tamaño de la letra en puntos
+                        fontWeight: FontWeight.bold, // Texto en negrita
+                      )
+                  ),
+                ),
+                Icon(
+                  Icons.car_repair,
+                  size: 70, // Tamaño del icono
+                  color: Colors.black, // Color del icono (opcional)
+                ),
+              ],
+            ),
+            SizedBox(height: 20,),
+            TextFormField(
+              controller: title,
+              onChanged:(value) => setState(() {title.text = value;}),
+              cursorColor: Theme.of(context).primaryColor,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: 'Titulo',
+                suffixIcon: title.text.length > 9 ?
+                Container(
+                    height: 30,
+                    width: 30,
+                    margin: const EdgeInsets.all(10.0),
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.green
+                    ),
+                    child: const Icon(
+                        Icons.done,
+                        color: Colors.white,
+                        size: 20.0
+                    )
+                ) : null,
+                border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12.0))
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+            SizedBox(height: 10,),
+            TextField(
+              maxLines: 4,
+              controller: description,
+              onChanged:(value) => setState(() {description.text = value;}),
+              cursorColor: Theme.of(context).primaryColor,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: 'Descripción',
+                suffixIcon: description.text.length > 9 ?
+                Container(
+                    height: 30,
+                    width: 30,
+                    margin: const EdgeInsets.all(10.0),
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.green
+                    ),
+                    child: const Icon(
+                        Icons.done,
+                        color: Colors.white,
+                        size: 20.0
+                    )
+                ) : null,
+                border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12.0))
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+            ),
+            SizedBox(height: 10,),
+            TextFormField(
+              controller: estimatedDate,
+              //onChanged:(value) => setState(() {estimatedDate.text = value;}),
+              cursorColor: Theme.of(context).primaryColor,
+              style: const TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: 'Fecha estimada',
+                prefixIcon: Icon(Icons.calendar_today),
+                suffixIcon: estimatedDate.text.length > 9 ?
+                Container(
+                    height: 30,
+                    width: 30,
+                    margin: const EdgeInsets.all(10.0),
+                    decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.green
+                    ),
+                    child: const Icon(
+                        Icons.done,
+                        color: Colors.white,
+                        size: 20.0
+                    )
+                ) : null,
+                border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12.0))
+                ),
+                filled: true,
+                fillColor: Colors.white,
+              ),
+              readOnly: true,
+              onTap: (){
+                _selectDate();
+              },
+            ),
+            Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                    onPressed: ()
+                    {
+                      String fechaString = estimatedDate.text;
+                      DateTime fecha = DateTime.parse(fechaString);
+                      String fechamilisegundos = fecha.millisecondsSinceEpoch.toString();
+                      String tiempoActual = DateTime.now().millisecondsSinceEpoch.toString();
+
+                      final data = {
+                        "carId": widget.carId,
+                        "description": description.text,
+                        "endDate": "",
+                        "estimatedDate": fechamilisegundos,
+                        "locationId": ap.administratorModel.locationId,
+                        "startDate": tiempoActual,
+                        "status": "EN PROGRESO",
+                        "title": title.text,
+                        "userId": widget.userId
+                      };
+
+                      _firebaseFirestore.collection("inspections").add(data)
+                          .then((documentReference) {
+                        print("Se añadió correctamente. ID del documento: ${documentReference.id}");
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const MainScreen(),
+                            ), (route) => false);
+
+                      })
+                          .catchError((error) {
+                        print("Ocurrió un error al añadir el documento: $error");
+                      });
+
+                      /*
+                      String phoneNumber = phoneController.text.trim();
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AddInspectionCar(
+                            numero: "+${selectedCountry.phoneCode}$phoneNumber",
+                          ),
+                        ),
+                      );*/
+                    },
+                    child:
+                    Text("Agregar Revisión")
+                )
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
